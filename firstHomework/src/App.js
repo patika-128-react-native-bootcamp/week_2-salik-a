7b6184d613e6c07087c28ddaf055420027be82d8
salik-a <salika@mef.edu.tr>
2021-11-24 20:36:28 +0300

    first commit

---
+++ firstHomework/src/App.js
@@ -0,0 +1,22 @@
+
+
+import React from 'react';
+import {
+  SafeAreaView,
+  Text,
+  View,
+} from 'react-native';
+
+
+
+const App = () => {
+
+  return (
+    <SafeAreaView >
+      <Text>App</Text>
+    </SafeAreaView>
+  );
+};
+
+
+export default App;
