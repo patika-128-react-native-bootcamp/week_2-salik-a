0c103ea8ac9a3b8a0f88e5c6661da0da426aab66
salik-a <salika@mef.edu.tr>
2021-11-25 21:16:02 +0300

    sort algorithm added

--- firstHomework/src/screens/MainScreen.js
+++ firstHomework/src/screens/MainScreen.js
@@ -1,9 +1,7 @@
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
     FlatList,
-    SafeAreaView,
-    Text,
     View,
 } from 'react-native';
 import styles from "./MainScreenStyle"
@@ -14,23 +12,42 @@ import SortButton from '../components/SortButton/SortButton';
 
 const Main = () => {
 
-    const [data, setData] = useState([])
+    const [data, setData] = useState([]);
+    const [sort, setSort] = useState("");
 
+    useEffect(() => {
+        console.log(sort)
+        sortData(sort)
+    }, [data])
 
     const renderItem = ({ item }) => (
         <ProductCard data={item} />
     )
 
-    const handleSortPress = () => {
 
+    const sortData = (text) => {
+        if (text === "increase") {
+            const sorted = data.sort((a, b) => b.price - a.price);
+            setData(sorted)
+            console.log(sort, sorted)
+        } else if (text === "decrease") {
+            const sorted = data.sort((a, b) => a.price - b.price);
+            setData(sorted)
+            console.log(sort, sorted)
+        } else {
+            const sorted = data.sort((a, b) => b.date - a.date);
+            setData(sorted)
+            console.log(sort, sorted)
+        }
+        setSort(text)
     }
 
     return (
         <View style={styles.container}>
             <View style={styles.header}>
-                <SortButton title="Artan Fiyat" onPress={handleSortPress} />
-                <SortButton title="Azalan Fiyat" onPress={handleSortPress} />
-                <SortButton title="Tarih" onPress={handleSortPress} />
+                <SortButton title="Artan Fiyat" onPress={() => sortData("increase")} />
+                <SortButton title="Azalan Fiyat" onPress={() => sortData("decrease")} />
+                <SortButton title="Tarih" onPress={() => sortData("history")} />
             </View>
             <FlatList
                 data={data}
